5dfd17a01a72df350fb4158bdd363869203390c9
meyeroliver <meyeroliver9401@gmail.com>
2020-05-03 10:50:56 +0200

    sorted out the user profile fragment layout

--- app/src/main/java/za/grounded/jwc_app/ui/activities/ProfileActivity.java
+++ app/src/main/java/za/grounded/jwc_app/ui/activities/ProfileActivity.java
@@ -1,10 +1,15 @@
 package za.grounded.jwc_app.ui.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
 import za.grounded.jwc_app.R;
+import za.grounded.jwc_app.ui.fragments.CartFragment;
+import za.grounded.jwc_app.ui.fragments.CheckOutFragment;
+import za.grounded.jwc_app.ui.fragments.UserProfileFragment;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -12,5 +17,11 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction
+                .replace(R.id.user_profile, new UserProfileFragment())
+                .commit();
     }
 }
---
+++ app/src/main/java/za/grounded/jwc_app/ui/fragments/UserProfileFragment.java
@@ -0,0 +1,24 @@
+package za.grounded.jwc_app.ui.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import za.grounded.jwc_app.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class UserProfileFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_user_profile, container, false);
+    }
+}
