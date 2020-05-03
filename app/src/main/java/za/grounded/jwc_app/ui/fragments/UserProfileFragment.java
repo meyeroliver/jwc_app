dae257dbf93cf2e8a3aa10a45922b06914735931
meyeroliver <meyeroliver9401@gmail.com>
2020-05-03 12:51:47 +0200

    starting to implement google places api. need to do it cost effectively

--- app/src/main/java/za/grounded/jwc_app/ui/fragments/UserProfileFragment.java
+++ app/src/main/java/za/grounded/jwc_app/ui/fragments/UserProfileFragment.java
@@ -2,11 +2,22 @@ package za.grounded.jwc_app.ui.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest;
+import com.google.android.libraries.places.api.net.PlacesClient;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
 import za.grounded.jwc_app.R;
 
@@ -15,10 +26,35 @@ import za.grounded.jwc_app.R;
  */
 public class UserProfileFragment extends Fragment {
 
+    private PlacesClient placesClient;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_user_profile, container, false);
+        this.initPlaces();
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Use fields to define the data types to return.
+        List<Place.Field> placeFields = Arrays.asList(Place.Field.NAME);
+
+        // Use the builder to create a FindCurrentPlaceRequest.
+        FindCurrentPlaceRequest request =
+                FindCurrentPlaceRequest.builder(placeFields)
+                        .build();
+    }
+
+    private void initPlaces() {
+        // Initialize Places.
+        Places.initialize(Objects.requireNonNull(getContext()), getString(R.string.places_api_key));
+
+        // Create a new Places client instance.
+        placesClient = Places.createClient(getContext());
     }
 }
